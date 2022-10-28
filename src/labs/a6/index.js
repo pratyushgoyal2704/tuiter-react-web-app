ba7c60c0b526c5a274374ef06980b9062d692a55
Pratyush Goyal <42108127+pratyushgoyal2704@users.noreply.github.com>
2022-10-28 11:00:43 -0700

    bug fix

--- src/labs/a6/index.js
+++ src/labs/a6/index.js
@@ -1,9 +1,6 @@
-import {Link} from "react-router-dom";
 import Classes from "./classes";
 import Styles from "./styles";
 import ConditionalOutput from "./conditional-output";
-
-import TodoItem from "./todo/todo-item";
 import TodoList from "./todo/todo-list";
 
 const Assignment6 = () => {
